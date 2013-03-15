3b25344491e08b960bc4e4e87c2a868678de7759
Sandile Keswa <sandile.keswa@gmail.com>
2013-03-15 19:31:00 -0400

    event bus invokeDelegate() need implementation.

--- org.pakhama.vaadin.mvp/src/main/java/org/pakhama/vaadin/mvp/event/impl/EventBus.java
+++ org.pakhama.vaadin.mvp/src/main/java/org/pakhama/vaadin/mvp/event/impl/EventBus.java
@@ -100,13 +100,24 @@ public class EventBus implements IEventBus {
 			}
 			break;
 		case UNIVERAL:
+			delegates = this.delegateRegistry.find(event.getClass());
 			break;
+		}
+		
+		if (delegates != null) {
+			for (IEventDelegate delegate : delegates) {
+				invokeDelegate(delegate);
+			}
 		}
 	}
 
 	@Override
 	public IEventDelegateRegistry getRegistry() {
 		return this.getRegistry();
+	}
+	
+	private void invokeDelegate(IEventDelegate delegate) {
+		// TODO:
 	}
 
 	private void injectField(Class<? extends Annotation> annotationType, Object instance, Object fieldValue) {
@@ -144,142 +155,5 @@ public class EventBus implements IEventBus {
 		throw new FieldInjectionException("No field in the class " + instance.getClass() + " was marked with the " + annotationType
 				+ " annotation. A field with this annotation is a requirement for injection to be successful.");
 	}
-
-	// private EventHandlerRegistry eventRegistry;
-	//
-	// private EventHandlerRegistry provideEventRegistry() {
-	// if (this.eventRegistry == null) {
-	// this.eventRegistry = new EventHandlerRegistry();
-	// }
-	//
-	// return this.eventRegistry;
-	// }
-	//
-	// @Override
-	// public void register(IPresenter<? extends IView> presenter) {
-	// if (presenter == null) {
-	// throw new
-	// IllegalArgumentException("The presenter parameter cannot be null.");
-	// }
-	//
-	// Method[] methods = presenter.getClass().getMethods();
-	// EventListener listener = null;
-	// for (Method method : methods) {
-	// if (method != null) {
-	// if ((listener = method.getAnnotation(EventListener.class)) != null) {
-	// if (listener.event() == null) {
-	// throw new EventListenerRegistrationException("Listener method " + method
-	// +
-	// " could not be registered because the event parameter of the @Listener annotation was null.");
-	// }
-	//
-	// Class<?>[] paramTypes = method.getParameterTypes();
-	// if (paramTypes.length > 1) {
-	// throw new EventListenerRegistrationException("Listener method " + method
-	// +
-	// " could not be registered because an event listener method can only have its own Event, or nothing, as a parameter.");
-	// } else if ((paramTypes.length == 1) &&
-	// !listener.event().isAssignableFrom(paramTypes[0])) {
-	// throw new EventListenerRegistrationException("Listener method " + method
-	// +
-	// " could not be registered because its only parameter did not match the event value of the @Listener parameter.");
-	// }
-	//
-	// provideEventRegistry().register(presenter, method, listener.event());
-	// }
-	// }
-	// }
-	// }
-	//
-	// @Override
-	// public void unregister(IPresenter<? extends IView> presenter) {
-	// if (presenter == null) {
-	// throw new
-	// IllegalArgumentException("The presenter parameter must not be null.");
-	// }
-	//
-	// provideEventRegistry().unregister(presenter);
-	// }
-	//
-	// @Override
-	// public void fire(Object source, Event e) {
-	// if (source == null) {
-	// throw new
-	// IllegalArgumentException("The source parameter cannot be null.");
-	// }
-	// if (e == null) {
-	// throw new
-	// IllegalArgumentException("The event parameter cannot be null.");
-	// }
-	//
-	// e.setSource(source);
-	// switch (e.getScope()) {
-	// case CHILDREN:
-	// if (source instanceof Presenter<?>) {
-	// try {
-	// provideEventRegistry().invokeChildren(e);
-	// } catch (Throwable t) {
-	// throw new EventListenerFaultException(t, e);
-	// }
-	// break;
-	// }
-	// break;
-	// case SIBLING:
-	// if (source instanceof Presenter<?>) {
-	// IPresenter<?> parent = ((Presenter<?>) source).getParent();
-	// if (parent != null) {
-	// try {
-	// provideEventRegistry().invokeSiblings(e, parent);
-	// } catch (Throwable t) {
-	// throw new EventListenerFaultException(t, e);
-	// }
-	// break;
-	// }
-	// }
-	// break;
-	// case PARENT:
-	// if (source instanceof Presenter<?>) {
-	// IPresenter<?> parent = ((Presenter<?>) source).getParent();
-	// if (parent != null) {
-	// try {
-	// provideEventRegistry().invokeParent(e, parent);
-	// } catch (Throwable t) {
-	// throw new EventListenerFaultException(t, e);
-	// }
-	// break;
-	// }
-	// } else if (source instanceof IView) {
-	// Object parent = ((IView) source).getOwner();
-	// if (parent != null) {
-	// try {
-	// provideEventRegistry().invokeParent(e, parent);
-	// } catch (Throwable t) {
-	// throw new EventListenerFaultException(t, e);
-	// }
-	// break;
-	// }
-	// }
-	// break;
-	// case ALL:
-	// default:
-	// try {
-	// provideEventRegistry().invokeAll(e);
-	// } catch (Throwable t) {
-	// throw new EventListenerFaultException(t, e);
-	// }
-	// break;
-	// }
-	// }
-	//
-	// @Override
-	// public void fire(Object source, EventScope scope, Event e) {
-	// if (scope == null) {
-	// throw new
-	// IllegalArgumentException("The scope parameter cannot be null.");
-	// }
-	//
-	// e.setScope(scope);
-	// fire(source, e);
-	// }
 
 }
--- org.pakhama.vaadin.mvp/src/main/java/org/pakhama/vaadin/mvp/exception/EventListenerInvocationException.java
+++ org.pakhama.vaadin.mvp/src/main/java/org/pakhama/vaadin/mvp/exception/EventListenerInvocationException.java
@@ -1,16 +1,24 @@
 package org.pakhama.vaadin.mvp.exception;
 
+import java.util.Collection;
+
 public class EventListenerInvocationException extends RuntimeException {
 	private static final long serialVersionUID = 215640467452512354L;
 
 	private String message;
+	private Collection<Throwable> causes;
 	
-	public EventListenerInvocationException(String message) {
+	public EventListenerInvocationException(String message, Collection<Throwable> throwables) {
+		this.causes = throwables;
 		this.message = message;
 	}
 	
 	@Override
 	public String getMessage() {
 		return message;
+	}
+	
+	public Collection<Throwable> getCauses() {
+		return causes;
 	}
 }
